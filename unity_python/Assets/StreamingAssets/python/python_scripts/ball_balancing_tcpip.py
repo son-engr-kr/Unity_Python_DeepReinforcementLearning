0acf2bc01c204484f5e166842a6653943fec3e0c
son_zephyrus_duo <son.engr.kr@gmail.com>
2023-12-11 16:39:02 +0900

    Implement multistep_returns

--- unity_python/Assets/StreamingAssets/python/python_scripts/ball_balancing_tcpip.py
+++ unity_python/Assets/StreamingAssets/python/python_scripts/ball_balancing_tcpip.py
@@ -31,10 +31,14 @@ class ReplayMemory:
 
     def push(self, state, action, next_state, reward):
         self.memory.append((state, action, next_state, reward))
-
+    def get_at_index(self, idx):
+        return self.memory[idx]
     def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
+        indices = random.sample(range(len(self.memory)), batch_size)
+        transitions = [self.memory[idx] for idx in indices]
+        return indices, transitions
+    def __getitem__(self, idx):
+        return self.memory[idx]
     def __len__(self):
         return len(self.memory)
 
@@ -88,24 +92,62 @@ class DQNAgent:
         else:
             return torch.tensor([[random.randrange(self.action_size)]], device=device, dtype=torch.long)
 
+    def compute_multistep_return(self, current_index, n_steps, gamma):
+        return_value = 0.0
+        for i in range(n_steps):
+            step_index = (current_index + i) % len(self.memory)
+            state, action, next_state, reward = self.memory[step_index]
+            return_value += (gamma ** i) * reward
+            if next_state is None:  # 에피소드가 종료된 경우
+                break  # 더 이상 미래 보상을 계산하지 않음
+        return return_value
     def optimize_model(self):
         if len(self.memory) < self.batch_size:
             return
-        transitions = self.memory.sample(self.batch_size)
+        '''
+        transitions = [
+            (state0, action0, next_state0, reward0),
+            (state1, action1, next_state1, reward1),
+            (state2, action2, next_state2, reward2)
+        ]
+        '''
+        indices, transitions = self.memory.sample(self.batch_size)
+        '''
+        Transition(
+            state=(state0, state1, state2),
+            action=(action0, action1, action2),
+            next_state=(next_state0, next_state1(None), next_state2),
+            reward=(reward0, reward1, reward2)
+        )
+        '''
         batch = Transition(*zip(*transitions))
-
+        #tensor([True, False, True]) (next_state가 None이면 False)
         non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
                                                 batch.next_state)), device=device, dtype=torch.bool)
+        # tensor([next_state0, next_state2]) - dim[2,input_len]
         non_final_next_states = torch.cat([s for s in batch.next_state
                                             if s is not None])
+        '''
+        batch_size가 128일때
+        state_batch = torch.cat([state0, state1, state2]) - dim[128*input_len]
+        action_batch = torch.cat([action0, action1, action2]) - dim[128 * action_len(1)]
+        reward_batch = torch.cat([reward0, reward1, reward2]) - dim[128 * reward_len(1)]
+        '''                                            
+                                            
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
+        # reward_batch = torch.cat(batch.reward) # one-step
 
+        multistep_returns = torch.tensor([self.compute_multistep_return(idx, 10, self.gamma)
+                                  for idx in indices], device=device)#multi-step
+
+        # dim[128, action_size(5)]
         state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+        # dim[128]
         next_state_values = torch.zeros(self.batch_size, device=device)
         next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
-        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+        # expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+        expected_state_action_values = multistep_returns
 
         loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
         self.optimizer.zero_grad()
